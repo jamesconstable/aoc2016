b7e91a1f2dbe2237888c6f1389aae1a413c55943
James Constable <jwdconstable@gmail.com>
2021-03-19 04:57:51 +1100

    Move rle and split_eqs into common for Day 6

--- 04.pl
+++ 04.pl
@@ -55,19 +55,6 @@ is_room(room(Name, _, Checksum)) :-
   pairs_values(RLESorted, FullChecksum),
   split_at(5, FullChecksum, Checksum, _).
 
-%% rle(+List, -RunLengths) is det
-%  RunLengths is the run-length encoding of List, expressed in pairs.
-%  For example, rle([a, a, b, c, c, c, a], X) -> X = [2-a, 1-b, 3-c, 1-a].
-rle([], []).
-rle(Xs, [L-E|Rs]) :- split_eqs(Xs, [E|Es], T), length([E|Es], L), rle(T, Rs).
-
-%% split_eqs(+List, -Eqs, -Rest) is det
-%  List is the concatenation of Eqs and Rest, where all elements of Eqs are
-%  unifiable.
-split_eqs([X], [X], []).
-split_eqs([X, X|Xs], [X|Eqs], Rest) :- split_eqs([X|Xs], Eqs, Rest).
-split_eqs([X1, X2|Xs], [X1], [X2|Xs]) :- X1 \= X2.
-
 %% split_at(?Index, ?List, ?Init, ?Tail)
 %  List is the concatenation of Init and Tail, where Init has length Index.
 split_at(0, Xs, [], Xs).
--- common.pl
+++ common.pl
@@ -3,3 +3,16 @@
 :- use_module(library(readutil)).
 
 read_input(Grammar) :- phrase_from_stream(Grammar, user_input).
+
+%% rle(+List, -RunLengths) is det
+%  RunLengths is the run-length encoding of List, expressed in pairs.
+%  For example, rle([a, a, b, c, c, c, a], X) -> X = [2-a, 1-b, 3-c, 1-a].
+rle([], []).
+rle(Xs, [L-E|Rs]) :- split_eqs(Xs, [E|Es], T), length([E|Es], L), rle(T, Rs).
+
+%% split_eqs(+List, -Eqs, -Rest) is det
+%  List is the concatenation of Eqs and Rest, where all elements of Eqs are
+%  unifiable.
+split_eqs([X], [X], []).
+split_eqs([X, X|Xs], [X|Eqs], Rest) :- split_eqs([X|Xs], Eqs, Rest).
+split_eqs([X1, X2|Xs], [X1], [X2|Xs]) :- X1 \= X2.
