3cd39da3c9b04e2046644967ffa52a0230139040
James Constable <jwdconstable@gmail.com>
2021-04-11 22:03:48 +1000

    Add solver for Day 18, part 2

--- 18.pl
+++ 18.pl
@@ -10,22 +10,29 @@ main(['1']) :- !, read_input(input_dcg(I)), part1(I, R), writeln(R).
 main(['2']) :- !, read_input(input_dcg(I)), part2(I, R), writeln(R).
 main(_)     :- writeln(user_error, 'Invalid part number. Must be 1 or 2.').
 
-part1(I, SafeCount) :-
-  iterate(40, line_succ, I, Lines),
-  maplist(count(=('.')), Lines, Counts),
-  sum_list(Counts, SafeCount).
+part1(I, C) :- safe_count_rows(I, 40, C).
+part2(I, C) :- safe_count_rows(I, 400000, C).
 
-part2(I, SafeCount) :-
-  iterate(400000, line_succ, I, Lines),
-  maplist(count(=('.')), Lines, Counts),
-  sum_list(Counts, SafeCount).
+safe_count_rows(I, NRows, SafeCount) :-
+  iterate(NRows,
+    [L-Acc, L1-Acc1]>>once((
+      count(=('.'), L, N),
+      line_succ(L, L1),
+      Acc1 #= Acc + N)),
+    I-0, _-SafeCount).
 
-iterate(0, _, _, []).
-iterate(N, Goal, I, [I|R]) :-
+%% iterate(+N, :Goal, ?I, ?R) is det
+%  R is the result of calling Goal N times, with I as the first argument of the
+%  first call, and thereafter using the second argument of each call as the
+%  first argument of the next.
+iterate(0, _, I, I).
+iterate(N, Goal, I, R) :-
   N #> 0, N1 #= N-1,
   call(Goal, I, I2),
   iterate(N1, Goal, I2, R).
 
+%% line_succ(+L, -S)
+%  S is the line following L according to the trap placement rules.
 line_succ(L, S) :- append(['.'|L], ['.'], WithWalls), line_succ_(WithWalls, S).
 
 line_succ_(['^', '^', '.'|T], ['^'|R]) :- line_succ_(['^', '.'|T], R).
